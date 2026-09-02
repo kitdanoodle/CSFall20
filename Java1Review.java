cc408299fd201fed4c397cffa22164a74f321e82
kitdanoodle <152928667+kitdanoodle@users.noreply.github.com>
2026-09-02 13:06:15 -0500

    saving everything

---
+++ Java1Review.java
@@ -0,0 +1,18 @@
+/**
+ * Implement each of the 10 methods tested in JUnitTests.java. Study the tests
+ * to determine how the methods should work.
+ */
+public class Java1Review {
+
+    public static void main(String[] args) {
+        // If you want to write your own tests, do so here. (Do not modify
+        // JUnitTests.java.) To run this method in Eclipse, right-click
+        // Java1Review.java in the Package Explorer and select "Run As" >
+        // "Java Application" from the context menu.
+    }
+    public static double divide(double num, double num2) {
+        double quotient = num / num2;
+        return quotient;
+    }
+
+}
